81e0ca493514df7f8fad0f61d51c642cc1b22c76
Iyanda Olamidotun <olamidotun225@gmail.com>
2024-12-12 19:32:04 +0100

    fix: errors

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -77,12 +77,12 @@ export class AuthService {
     response.cookie('access_token', accessToken, {
       httpOnly: true,
       sameSite: 'none',  // Allow cross-origin requests
-      secure: process.env.NODE_ENV === 'production',  // Ensure cookies are only sent over HTTPS in production
+      secure: this.configService.get<string>('NODE_ENV') === 'production',  // Ensure cookies are only sent over HTTPS in production
     });
 
     response.cookie('refresh_token', refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',  // Ensure cookies are only sent over HTTPS in production
+      secure: this.configService.get<string>('NODE_ENV') === 'production',  // Ensure cookies are only sent over HTTPS in production
       sameSite: 'none',  // Allow cross-origin requests
     });
 
