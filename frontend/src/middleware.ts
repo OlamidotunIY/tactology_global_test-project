9c5630bafbfa847c5a9d2be16cb5b44eceb81c24
Iyanda Olamidotun <olamidotun225@gmail.com>
2024-12-12 22:10:21 +0100

    fix: errors

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,25 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("access_token")?.value; // Assume the token is stored in a cookie
-  const protectedPaths = ["/dashboard"];
+  const token = request.cookies.get("access_token")?.value;
 
-  // Redirect based on the token status and the current path
-  if (request.nextUrl.pathname === "/") {
-    if (token) {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    } else {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+  if (request.nextUrl.pathname === "/" && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  // Check if the pathname starts with any of the protected paths
-  if (
-    protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path)) &&
-    !token
-  ) {
-    return NextResponse.redirect(new URL("/login", request.url)); // Redirect to login if token is not present
+  if (request.nextUrl.pathname === "/" && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  return NextResponse.next(); // Allow the request if the user is authenticated or the path is not protected
+  if (request.nextUrl.pathname.startsWith("/dashboard") && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
 }
