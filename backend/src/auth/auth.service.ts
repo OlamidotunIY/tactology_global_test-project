0de58561cce7359cfa3bfca69689ea9aa5ae81a0
Iyanda Olamidotun <olamidotun225@gmail.com>
2024-12-12 19:47:41 +0100

    fix: errors

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -76,12 +76,14 @@ export class AuthService {
 
     response.cookie('access_token', accessToken, {
       httpOnly: true,
-      // sameSite: 'none',  // Allow cross-origin requests
+      sameSite: 'none',  // Allow cross-origin requests
+      secure: this.configService.get<string>('NODE_ENV') === 'production',  // Ensure cookies are only sent over HTTPS in production
     });
 
     response.cookie('refresh_token', refreshToken, {
       httpOnly: true,
-      // sameSite: 'none',  // Allow cross-origin requests
+      secure: this.configService.get<string>('NODE_ENV') === 'production',  // Ensure cookies are only sent over HTTPS in production
+      sameSite: 'none',  // Allow cross-origin requests
     });
 
     return { user };
