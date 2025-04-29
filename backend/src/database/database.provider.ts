f1869fdbd24a87b8f2669f91183ba994e8d9c1cb
Iyanda Olamidotun <olamidotun225@gmail.com>
2025-04-29 18:53:59 +0100

    fix:migration

--- backend/src/database/database.provider.ts
+++ backend/src/database/database.provider.ts
@@ -10,7 +10,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: process.env.NODE_ENV === 'production' ? false : true, // Set to false in production
+  synchronize: true, // Set to false in production
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
 });
