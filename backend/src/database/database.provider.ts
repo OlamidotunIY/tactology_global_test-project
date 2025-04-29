fb15974534bb09e76c353de0c171ea8b65b9b72e
Iyanda Olamidotun <olamidotun225@gmail.com>
2025-04-29 18:39:19 +0100

    fix:migration

--- backend/src/database/database.provider.ts
+++ backend/src/database/database.provider.ts
@@ -1,28 +1,30 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: process.env.NODE_ENV === 'production' ? false : true, // Set to false in production
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+});
+
+// Use this for NestJS DI system
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
-      try {
-        const dataSource = new DataSource({
-          type: 'postgres',
-          host: process.env.DB_HOST,
-          port: parseInt(process.env.DB_PORT, 10) || 5432,
-          username: process.env.DB_USERNAME,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
-          synchronize: process.env.NODE_ENV === 'production' ? false : true, // Set to false in production
-          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        });
-
-        await dataSource.initialize();
-        console.log('Database connection established successfully');
-        return dataSource;
-      } catch (error) {
-        console.error('Database connection failed:', error.message);
-        throw error;
+      if (!AppDataSource.isInitialized) {
+        await AppDataSource.initialize();
       }
+      console.log('Database connection established successfully');
+      return AppDataSource;
     },
   },
 ];
