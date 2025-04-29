6ab68529dbe8867601a2f0f0af62cd4f4affc6ce
Iyanda Olamidotun <olamidotun225@gmail.com>
2025-04-29 18:02:00 +0100

    fix: error

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("access_token")?.value;
+  const token = request.cookies.get("access_token")
 
   if (request.nextUrl.pathname === "/" && token) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
